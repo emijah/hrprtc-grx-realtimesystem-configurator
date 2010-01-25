23e60aee46a6a24f4e1d1c575e4987ca65486b13
kawasumi <kawasumi@generalrobotix.com>
2010-01-25 16:07:50 +0900

    add listview and perspective

---
+++ src/com/generalrobotix/ui/realtimesystem_configurator/PerspectiveFactory.java
@@ -0,0 +1,10 @@
+package com.generalrobotix.ui.realtimesystem_configurator;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class PerspectiveFactory implements IPerspectiveFactory {
+	public void createInitialLayout(IPageLayout layout) {
+		
+	}
+}
