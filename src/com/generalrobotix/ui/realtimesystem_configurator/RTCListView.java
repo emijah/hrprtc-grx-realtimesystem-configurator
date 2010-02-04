614a61223269e23f596038e8017e21a915b54836
Akira Nishitarumizu <nisitaru@generalrobotix.com>
2010-02-05 00:05:47 +0900

    ???????????????

---
+++ src/com/generalrobotix/ui/realtimesystem_configurator/RTCListView.java
@@ -0,0 +1,328 @@
+package com.generalrobotix.ui.realtimesystem_configurator;
+
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerSorter;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ViewPart;
+import org.openrtp.repository.ProfileValidateException;
+import org.openrtp.repository.RTSystemProfileOperator;
+import org.openrtp.repository.xsd.rtsystem.Component;
+import org.openrtp.repository.xsd.rtsystem.RtsProfile;
+
+public class RTCListView extends ViewPart {
+	private TableViewer viewer;
+	private Action action1;
+	private Action action2;
+	private Action doubleClickAction;
+
+    public RTSystemProfileOperator rtsProfileOperator = new RTSystemProfileOperator();
+    private RtsProfile profile; 
+    
+	private NullProgressMonitor progress;
+    
+	/**
+	 * The constructor.
+	 */
+	public RTCListView() {
+	}
+
+	/**
+	 * This is a callback that will allow us
+	 * to create the viewer and initialize it.
+	 */
+	public void createPartControl(Composite parent) {
+		GridLayout layout = new GridLayout(1, false);
+		parent.setLayout(layout);
+		
+		Button btnLoad = new Button(parent, SWT.NONE);
+		btnLoad.setText("Load RTSystem Profile");
+		btnLoad.addSelectionListener(new SelectionListener() {
+			public void widgetDefaultSelected(SelectionEvent e) {}
+
+			public void widgetSelected(SelectionEvent e) {
+				FileDialog fdlg = new FileDialog(((Button)e.getSource()).getShell(), SWT.OPEN);
+				fdlg.setFilterExtensions( new String[] {"*.xml"} );
+				fdlg.open();
+				String fname = fdlg.getFileName();
+				if ( fname != null && fname != "" ) {
+					profile = getRTSProfile(fdlg.getFilterPath()+java.io.File.separator+fname);
+					viewer.refresh();
+					try {
+						createProjectDir(fname.replace(".xml", ""));
+					} catch (CoreException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+		});		
+		
+		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION);
+		viewer.setContentProvider(new ViewContentProvider());
+		viewer.setLabelProvider(new ViewLabelProvider());
+		viewer.setInput(new Object[]{});
+		Table table = viewer.getTable();
+		GridData tableLayoutData = new GridData(GridData.FILL_BOTH);
+		table.setLayoutData(tableLayoutData);
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		
+		TableColumn column = new TableColumn(table, SWT.LEFT, 0);
+		column.setText("RTC Instance");
+		column.setWidth(200);
+		
+		column = new TableColumn(table, SWT.LEFT, 1);
+		column.setText("EC ID");
+		column.setWidth(150);
+		
+		column = new TableColumn(table, SWT.LEFT, 2);
+		column.setText("RealTime");
+		column.setWidth(100);
+		
+		column = new TableColumn(table, SWT.LEFT, 3);
+		column.setText("test1");
+		column.setWidth(100);
+		
+		column = new TableColumn(table, SWT.LEFT, 4);
+		column.setText("test2");
+		column.setWidth(100);
+		
+		makeActions();
+		hookContextMenu();
+		hookDoubleClickAction();
+		contributeToActionBars();
+	}
+	
+	public void createProjectDir(String projectName) throws CoreException {
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IProject project = root.getProject(projectName);
+		if ( !project.exists() ) {
+			project.create(progress);
+			project.open(progress);
+		}
+		
+		IFolder folderScripts = project.getFolder("scripts");
+		if ( !folderScripts.exists() ) {
+			folderScripts.create(true, true, progress);
+		}
+		
+		IFolder folderIdls = project.getFolder("idls");
+		if ( !folderIdls.exists() ) {
+			folderIdls.create(true, true, progress);
+		}
+
+		IFile file = project.getFile("scripts/rtc.conf");
+		if ( !file.exists() ) {
+			file.create(new ByteArrayInputStream(new byte[0]), true, progress);
+		}
+	}
+	
+	private void hookContextMenu() {
+		MenuManager menuMgr = new MenuManager("#PopupMenu");
+		menuMgr.setRemoveAllWhenShown(true);
+		menuMgr.addMenuListener(new IMenuListener() {
+			public void menuAboutToShow(IMenuManager manager) {
+				RTCListView.this.fillContextMenu(manager);
+			}
+		});
+		Menu menu = menuMgr.createContextMenu(viewer.getControl());
+		viewer.getControl().setMenu(menu);
+		getSite().registerContextMenu(menuMgr, viewer);
+	}
+
+	private void contributeToActionBars() {
+		IActionBars bars = getViewSite().getActionBars();
+		fillLocalPullDown(bars.getMenuManager());
+		fillLocalToolBar(bars.getToolBarManager());
+	}
+
+	private void fillLocalPullDown(IMenuManager manager) {
+		manager.add(action1);
+		manager.add(new Separator());
+		manager.add(action2);
+	}
+
+	private void fillContextMenu(IMenuManager manager) {
+		manager.add(action1);
+		manager.add(action2);
+		// Other plug-ins can contribute there actions here
+		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+	}
+	
+	private void fillLocalToolBar(IToolBarManager manager) {
+		manager.add(action1);
+		manager.add(action2);
+	}
+
+	private void makeActions() {
+		action1 = new Action() {
+			public void run() {
+				showMessage("Action 1 executed");
+			}
+		};
+		action1.setText("Action 1");
+		action1.setToolTipText("Action 1 tooltip");
+		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
+			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+		
+		action2 = new Action() {
+			public void run() {
+				showMessage("Action 2 executed");
+			}
+		};
+		action2.setText("Action 2");
+		action2.setToolTipText("Action 2 tooltip");
+		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
+				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+		doubleClickAction = new Action() {
+			public void run() {
+				ISelection selection = viewer.getSelection();
+				Object obj = ((IStructuredSelection)selection).getFirstElement();
+				showMessage("Double-click detected on "+obj.toString());
+			}
+		};
+	}
+
+	private void hookDoubleClickAction() {
+		viewer.addDoubleClickListener(new IDoubleClickListener() {
+			public void doubleClick(DoubleClickEvent event) {
+				doubleClickAction.run();
+			}
+		});
+	}
+	private void showMessage(String message) {
+		MessageDialog.openInformation(
+			viewer.getControl().getShell(),
+			"Sample View",
+			message);
+	}
+
+	/**
+	 * Passing the focus request to the viewer's control.
+	 */
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+	
+    public RtsProfile getRTSProfile(String fname) {
+    	if ( fname != null)  {
+            try {
+    			rtsProfileOperator.loadProfile(fname);
+    	        return rtsProfileOperator.getRtsProfile();
+    		} catch (ProfileValidateException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		} catch (Exception e) {
+    			e.printStackTrace();
+    		}
+    	}
+		return null;
+    }
+    
+	/*
+	 * The content provider class is responsible for
+	 * providing objects to the view. It can wrap
+	 * existing objects in adapters or simply return
+	 * objects as-is. These objects may be sensitive
+	 * to the current input of the view, or ignore
+	 * it and always show the same content 
+	 * (like Task List, for example).
+	 */
+	 
+	class ViewContentProvider implements IStructuredContentProvider {
+		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
+		}
+		public void dispose() {
+		}
+		public Object[] getElements(Object parent) {
+			if ( profile != null) {
+				return profile.getComponent().toArray();
+			}
+			return new Object[]{};
+		}
+	}
+	
+	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
+		public String getColumnText(Object obj, int index) {
+			Component comp = (Component)obj;
+			switch(index) {
+			case 0:
+				return comp.getInstanceName();
+			case 1:
+				return comp.getCompositeType();
+			case 2:
+				return comp.getPathUri();
+			case 3:
+				return comp.getId();
+			default:
+				break;
+			}
+			return getText(obj);
+		}
+		public Image getColumnImage(Object obj, int index) {
+			return getImage(obj);
+		}
+		public Image getImage(Object obj) {
+			return null;//PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
+		}
+	}
+	class NameSorter extends ViewerSorter {
+	}
+    
+    public class SynchronizeListExportWithProgress implements IRunnableWithProgress {
+        public void run(IProgressMonitor monitor) throws InvocationTargetException,
+                InterruptedException {
+
+            monitor.beginTask("処理中...", IProgressMonitor.UNKNOWN);
+            
+            Thread.sleep(1000);
+            
+            monitor.done();
+        }
+    }
+}
