440cac1bbbd1b4dcae37d092294702987005819f
kawasumi <kawasumi@generalrobotix.com>
2010-02-06 00:35:49 +0900

    change view name

--- src/com/generalrobotix/ui/realtimesystem_configurator/BenchmarkOperatorView.java
+++ src/com/generalrobotix/ui/realtimesystem_configurator/BenchmarkOperatorView.java
@@ -3,22 +3,18 @@ package com.generalrobotix.ui.realtimesystem_configurator;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
-public class BenchTestOperatorView extends ViewPart {
+public class BenchmarkOperatorView extends ViewPart {
 
-	public BenchTestOperatorView() {
-		// TODO Auto-generated constructor stub
+	public BenchmarkOperatorView() {
 	}
 
 	@Override
 	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void setFocus() {
-		// TODO Auto-generated method stub
-
 	}
 
 }
