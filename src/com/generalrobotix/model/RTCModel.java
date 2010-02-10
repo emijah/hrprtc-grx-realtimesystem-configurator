d1e07182ef9e4de42c89b1178db83c27f764634c
Akira Nishitarumizu <nisitaru@generalrobotix.com>
2010-02-10 09:39:23 +0900

    add ChartComposite to TimingChartView which outputs XYStepAreaChart, change color settings of chart

---
+++ src/com/generalrobotix/model/RTCModel.java
@@ -0,0 +1,111 @@
+package com.generalrobotix.model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.TreeItem;
+import org.openrtp.repository.ProfileValidateException;
+import org.openrtp.repository.RTSystemProfileOperator;
+import org.openrtp.repository.xsd.rtsystem.Component;
+import org.openrtp.repository.xsd.rtsystem.RtsProfile;
+import org.openrtp.repository.xsd.rtsystem.TargetComponent;
+
+public class RTCModel {
+	private String nodeName;
+	private RTCModel parent;
+	private RTCModel top;
+	private List<RTCModel> children = new ArrayList<RTCModel>();
+	
+	private RtsProfile profile;
+	private Component component;
+	
+    public static RTSystemProfileOperator rtsProfileOperator = new RTSystemProfileOperator();
+	
+	public RTCModel(String rtsProfilePath) {
+	   	this.top = this;
+		load(rtsProfilePath);
+	}
+	
+    private RTCModel(RTCModel top, Component comp) {
+    	this.component = comp;
+    	this.nodeName = comp.getInstanceName();
+    }
+	
+	private void load(String fname) {
+	   	try {
+	   		rtsProfileOperator.loadProfile(fname);
+	   		profile = rtsProfileOperator.getRtsProfile();
+	   	} catch (ProfileValidateException e) {
+	   		e.printStackTrace();
+	   	}
+	   	
+    	nodeName = fname;
+ 
+		Iterator<Component> it = profile.getComponent().iterator();
+		while ( it.hasNext() ) {
+			this.add(new RTCModel(this, it.next()));
+		}
+		this.updateStructure();
+	}
+	
+	public String getName() {
+		return nodeName;
+	}
+	
+	public void add(RTCModel model) {
+		children.add(model);
+		if ( model.parent != null ) {
+			model.parent.children.remove(model);
+		}
+		model.parent = this;
+	}
+	
+	public RTCModel getParent() {
+		return parent;
+	}
+	
+	public List<RTCModel> getChildren () {
+		 return children;
+	}
+	
+	public Component getComponent() {
+		return component;
+	}
+	
+	public RTCModel find(String id, String instanceName) {
+		if ( component != null && component.getId().equals(id) && component.getInstanceName().equals(instanceName) ) {
+			return this;
+		}
+		Iterator<RTCModel> it = children.iterator();
+		while ( it.hasNext()) {
+			RTCModel model = it.next();
+			RTCModel ret = model.find(id, instanceName);
+			if ( ret != null) {
+				return ret;
+			}
+		}
+		return null;
+	}
+	
+	/*
+	 * update model structure
+	 */
+	private void updateStructure() {
+		for (int i=0;i<children.size(); i++) {
+			RTCModel model = children.get(i);
+			Component comp = model.component;
+	 		if ( comp.getCompositeType().equals("PeriodicECShared") || comp.getCompositeType().equals("PeriodicStateShared")) {
+	 			Iterator<TargetComponent> it2 = comp.getParticipants().getParticipant().iterator();
+	 			while ( it2.hasNext() ) {
+	 				TargetComponent tcomp = it2.next();
+	 				RTCModel result = top.find(tcomp.getComponentId(), tcomp.getInstanceName());
+	 				if ( result != null && !model.children.contains(result)) {
+	 					model.add(result);
+	 				}
+	 			}
+	 		}
+		}
+	}
+}
---
+++ src/com/generalrobotix/ui/realtimesystem_configurator/BenchmarkOperatorView.java
@@ -0,0 +1,20 @@
+package com.generalrobotix.ui.realtimesystem_configurator;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class BenchmarkOperatorView extends ViewPart {
+
+	public BenchmarkOperatorView() {
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		
+	}
+
+	@Override
+	public void setFocus() {
+	}
+
+}
