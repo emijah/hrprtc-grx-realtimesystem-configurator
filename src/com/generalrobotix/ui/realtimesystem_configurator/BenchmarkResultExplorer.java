ce5b366122d7f318c4cdd4443119011303f5e0d9
kawasumi <kawasumi@generalrobotix.com>
2010-02-17 19:49:54 +0900

    add icons

--- src/com/generalrobotix/ui/realtimesystem_configurator/BenchmarkResultExplorer.java
+++ src/com/generalrobotix/ui/realtimesystem_configurator/BenchmarkResultExplorer.java
@@ -11,6 +11,7 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -24,6 +25,11 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.viewers.CheckStateChangedEvent;
 import org.eclipse.jface.viewers.CheckboxTreeViewer;
 import org.eclipse.jface.viewers.ICheckStateListener;
@@ -32,20 +38,19 @@ import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeColumn;
-import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.ho.yaml.Yaml;
 
+import com.generalrobotix.model.BenchmarkResultModel;
 import com.generalrobotix.model.RTCModel;
 import com.generalrobotix.model.RTSystemItem;
 import com.generalrobotix.model.TreeModelItem;
@@ -55,7 +60,9 @@ public class BenchmarkResultExplorer extends ViewPart {
 	private TreeModelItem rootItem = new TreeModelItem();
 	private CheckboxTreeViewer resultViewer;
 	private NullProgressMonitor progress;
-	
+	private FileDialog fdlg;
+	private List<Action> actionList = new ArrayList<Action>();
+
 	public BenchmarkResultExplorer() {
 	}
 
@@ -63,40 +70,36 @@ public class BenchmarkResultExplorer extends ViewPart {
 	public void createPartControl(Composite parent) {
 		parent.setLayout(new GridLayout(1, false));
 		
+		fdlg = new FileDialog(parent.getShell(), SWT.OPEN);
+		
 		resultViewer = setupTreeViewer(parent);
 		getSite().setSelectionProvider(resultViewer);
-
-		Button btnImportSystem = new Button(parent, SWT.NONE);
-		btnImportSystem.setText("Import RTSystem Profile");
-		btnImportSystem.addSelectionListener(new SelectionListener() {
-			public void widgetDefaultSelected(SelectionEvent e) {}
-
-			public void widgetSelected(SelectionEvent e) {
-				FileDialog fdlg = new FileDialog(((Button)e.getSource()).getShell(), SWT.OPEN);
-				fdlg.setFilterExtensions( new String[] {"*.xml"} );
-				fdlg.open();
-				String fname = fdlg.getFilterPath() + java.io.File.separator + fdlg.getFileName();
-				importProfile(fname);
-			}
-		});
 		
-		Button btnSaveResult = new Button(parent, SWT.NONE);
-		btnSaveResult.setText("Save All Benchmark Results");
-		btnSaveResult.addSelectionListener(new SelectionListener() {
-			public void widgetDefaultSelected(SelectionEvent e) {}
-			public void widgetSelected(SelectionEvent e) {
-				save();
+		actionList.add(new ImportAction());
+		actionList.add(new SaveAction());
+
+		MenuManager menuManager = new MenuManager("#PopupMenu");
+		menuManager.setRemoveAllWhenShown(true);
+		menuManager.addMenuListener(new IMenuListener() {
+			public void menuAboutToShow(IMenuManager manager) {
+				Iterator<Action> it = actionList.iterator();
+				while (it.hasNext()) {
+					manager.add(it.next());
+				}
 			}
 		});
+		Menu menu = menuManager.createContextMenu(resultViewer.getControl());
+		resultViewer.getControl().setMenu(menu);
+		getSite().registerContextMenu(menuManager, resultViewer);
 		
-		Button btnLoadResult = new Button(parent, SWT.NONE);
-		btnLoadResult.setText("Load Benchmark Results");
-		btnLoadResult.addSelectionListener(new SelectionListener() {
-			public void widgetDefaultSelected(SelectionEvent e) {}
-			public void widgetSelected(SelectionEvent e) {
-				load();
-			}
-		});
+		IMenuManager manager1 = getViewSite().getActionBars().getMenuManager();
+		IToolBarManager manager2 = getViewSite().getActionBars().getToolBarManager();
+		Iterator<Action> it = actionList.iterator();
+		while (it.hasNext()) {
+			Action action = it.next();
+			manager1.add(action);
+			manager2.add(action);
+		}
 		
 		// TODO show dialog to confirm create project
 		project = getProject("RealtimeSystemConfiguratorRepository");
@@ -134,9 +137,35 @@ public class BenchmarkResultExplorer extends ViewPart {
 				rootItem.add(rts);
 			}
 		}
+		load();
 		resultViewer.refresh();
 	}
 	
+	private class ImportAction extends Action {
+		public ImportAction() {
+			setText("Import RTSystem Profile");
+			setImageDescriptor(AbstractUIPlugin.imageDescriptorFromPlugin(getSite().getPluginId(), "icons/folder_open.png"));
+		}
+		public void run() {
+			fdlg.setFilterExtensions( new String[] {"*.xml"} );
+			fdlg.open();
+			String fname = fdlg.getFilterPath() + java.io.File.separator + fdlg.getFileName();
+			if ( fname != null ) {
+				importProfile(fname);
+			}
+		}
+	};
+	
+	private class SaveAction extends Action {
+		public SaveAction() {
+			setText("Save All Benchmark Results");
+			setImageDescriptor(AbstractUIPlugin.imageDescriptorFromPlugin(getSite().getPluginId(), "icons/save.png"));
+		}
+		public void run() {
+			save();
+		}
+	};	
+
 	class ViewContentProvider implements ITreeContentProvider {
 
 		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
@@ -172,12 +201,15 @@ public class BenchmarkResultExplorer extends ViewPart {
 				case 1:
 					if ( item instanceof RTSystemItem ) {
 						return ((RTSystemItem)item).getVersion();
-					} else {
-						return "-";
 					}
-				case 2:
 					return "-";
-				case 3:
+				case 2:
+					if ( item instanceof RTCModel ) {
+						BenchmarkResultModel result = ((RTCModel)item).getResult();
+						if ( result.date != null ) {
+							return result.date.toString();
+						}
+					} else
 					return "-";
 				default:
 					break;
@@ -220,14 +252,12 @@ public class BenchmarkResultExplorer extends ViewPart {
 		
 		return viewer;
 	}
-
 	
 	private void setHeader(Tree tree, int alignment, int index, int width, String text) {
 		TreeColumn column = new TreeColumn(tree, alignment, index);
 		column.setText(text);
 		column.setWidth(width);
 	}
-	
 
 	private void importProfile(String fname) {
 		if ( fname != null && fname != "" ) {
